b161c7e2948d13822acc821b5f6fd2b922615a5a
gunjianpan <iofu728@163.com>
2018-09-08 22:14:17 +0800

    add pdd summary

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,7 +24,7 @@ module.exports = {
       {text: 'Other', link: '/other/gitSkill'},
     ],
     sidebar: {
-      '/other/': ['gitSkill', 'cs'],
+      '/other/': ['gitSkill', 'pdd', 'cs'],
       '/pat/': [
         '',
         {
