a3e51dd6b748e28959efe47f7e4414290679969a
gunjianpan <iofu728@163.com>
2018-09-10 00:32:42 +0800

    fix pv.sh

--- build.sh
+++ build.sh
@@ -27,10 +27,10 @@ else
 rm -rf /usr/local/var/www/pat
 fi
 
-sed -i '/<head>/a\<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />' /usr/local/var/www/wyydsb/blog/docs/.vuepress/dist/index.html
-sed -i '/<head>/a\<link rel="shortcut icon" href="https://cdn.nlark.com/yuque/0/2018/png/104214/1534957905839-d580e42e-3899-4403-be32-c068e5c9eef4.png">' /usr/local/var/www/wyydsb/blog/docs/.vuepress/dist/index.html
-sed -i '/<body>/a\<script type="text/javascript"> docsearch({ apiKey: 'c42b71d494ca78750c7094eb2c55eda6', indexName: 'wyydsb', inputSelector: '###REPLACEME####', debug: false }); </script>' /usr/local/var/www/wyydsb/blog/docs/.vuepress/dist/index.html
-sed -i '/<body>/a\<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>' /usr/local/var/www/wyydsb/blog/docs/.vuepress/dist/index.html
+sed -i '/<head>/a\<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />' docs/.vuepress/dist/index.html
+sed -i '/<head>/a\<link rel="shortcut icon" href="https://cdn.nlark.com/yuque/0/2018/png/104214/1534957905839-d580e42e-3899-4403-be32-c068e5c9eef4.png">' docs/.vuepress/dist/index.html
+sed -i '/<body>/a\<script type="text/javascript"> docsearch({ apiKey: 'c42b71d494ca78750c7094eb2c55eda6', indexName: 'wyydsb', inputSelector: '###REPLACEME####', debug: false }); </script>' docs/.vuepress/dist/index.html
+sed -i '/<body>/a\<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>' docs/.vuepress/dist/index.html
 
-mv /usr/local/var/www/wyydsb/blog/docs/.vuepress/dist/* /usr/local/var/www/
+mv docs/.vuepress/dist/* /usr/local/var/www/
 
