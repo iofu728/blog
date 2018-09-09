ccbd7f5ddec2fe3743d068b55702d70c60575650
gunjianpan <iofu728@163.com>
2018-09-09 21:42:43 +0800

    add pv.sh

---
+++ pv.sh
@@ -0,0 +1,10 @@
+echo '<center>累计访问量:' > pv
+awk '{print $1}' /usr/local/nginx/logs/access.log|sort | uniq -c |wc -l >> pv
+echo '| 昨日访问量:' >> pv
+yMonth=`date -d yesterday +%B`
+month=`date +%B`
+sed -n "/$(date -d yesterday +%d)\/${yMonth:0:3}\/$(date -d yesterday +%Y):00/,/$(date +%d)\/${month:0:3}\/$(date +%Y):00/p" /usr/local/nginx/logs/access.log | awk '{print $1}' | sort | uniq -c | wc -l >> pv
+echo '</center>' >> pv
+sed -i '$d' docs/README.md
+echo $(cat pv) >> docs/README.md
+bash build.sh
