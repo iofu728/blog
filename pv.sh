041ea6974683250e084c6b8f2cd562aacd155b76
gunjianpan <iofu728@163.com>
2018-09-09 22:17:43 +0800

    change pv.sh

--- pv.sh
+++ pv.sh
@@ -5,6 +5,6 @@ yMonth=`date -d yesterday +%B`
 month=`date +%B`
 sed -n "/$(date -d yesterday +%d)\/${yMonth:0:3}\/$(date -d yesterday +%Y):00/,/$(date +%d)\/${month:0:3}\/$(date +%Y):00/p" /usr/local/nginx/logs/access.log | awk '{print $1}' | sort | uniq -c | wc -l >> pv
 echo '</center>' >> pv
-sed -i '$d' docs/README.md
-echo $(cat pv) >> docs/README.md
-bash build.sh
+sed -i '$d' /usr/local/var/www/wyydsb/blog/docs/README.md
+echo $(cat pv) >> /usr/local/var/www/wyydsb/blog/docs/README.md
+bash /usr/local/var/www/wyydsb/blog/build.sh
