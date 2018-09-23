e4c1e988d0516c92fe316cd42a433ab15fb63dd2
gunjianpan <iofu728@163.com>
2018-09-23 19:47:14 +0800

    nginx change

--- day.sh
+++ day.sh
@@ -5,8 +5,8 @@ awk '{if($9==200)print $0}' /var/log/nginx/access.log|sed -n "/$(date -d yesterd
 echo $(cat yesterday) >> day
 
 
-# first=20180720
-# second=20180912
+# first=20180820
+# second=20180923
 
 # while [ "$first" != "$second" ]
 # do
@@ -14,7 +14,7 @@ echo $(cat yesterday) >> day
 # date -d "${first}" +%Y%m%d > yesterday
 # yMonth=`date -d "${first}" +%B`
 # month=`date -d "-1 days ago ${first}" +%B`
-# sed -n "/$(date -d "${first}" +%d)\/${yMonth:0:3}\/$(date -d "${first}" +%Y):00/,/$(date -d "-1 days ago ${first}" +%d)\/${month:0:3}\/$(date -d "-1 days ago ${first}" +%Y):00/p" /usr/local/nginx/logs/access.log | awk '{print $1}' | sort | uniq -c | wc -l >> yesterday
+# awk '{if($9==200)print $0}' /var/log/nginx/access.log|sed -n "/$(date -d "${first}" +%d)\/${yMonth:0:3}\/$(date -d "${first}" +%Y):00/,/$(date -d "-1 days ago ${first}" +%d)\/${month:0:3}\/$(date -d "-1 days ago ${first}" +%Y):00/p"| awk '{print $1}' | sort | uniq -c | wc -l >> yesterday
 # echo $(cat yesterday) >> day
 # let first=`date -d "-1 days ago ${first}" +%Y%m%d`
 # done
