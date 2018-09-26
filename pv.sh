9a2ac0b9799b8843137e4c81ee8168204023941e
gunjianpan <iofu728@163.com>
2018-09-26 14:59:08 +0800

    change pv.sh

--- pv.sh
+++ pv.sh
@@ -9,10 +9,10 @@ month=`date +%B`
 awk '{if($9==200)print $0}' $loglocal|sed -n "/$(date -d yesterday +%d)\/${yMonth:0:3}\/$(date -d yesterday +%Y)/,/$(date -d yesterday +%d)\/${yMonth:0:3}\/$(date -d yesterday +%Y)/p"| awk '{print $1}' | sort | uniq -c | wc -l >> log/pv
 echo '</center>' >> log/pv
 sed -i '$d' docs/README.md
-echo $(cat pv) >> docs/README.md
+echo $(cat log/pv) >> docs/README.md
 bash build.sh
 
 # time total Pv
 date +%Y%m%d%T > log/time
 awk '{if($9==200)print $0}' $loglocal|awk '{print $1}'|sort | uniq -c |wc -l >> log/time
-echo $(cat time) >> log/total
+echo $(cat log/time) >> log/total
