b56c5e1d8d2eb43b0d73b19070ae6dd7774eadf7
gunjianpan <iofu728@163.com>
2018-09-26 14:44:55 +0800

    change pv.sh

--- pv.sh
+++ pv.sh
@@ -6,7 +6,7 @@ awk '{if($9==200)print $0}' $loglocal|awk '{print $1}'|sort | uniq -c |wc -l >> 
 echo '| ×òÈÕ·ÃÎÊÁ¿:' >> pv
 yMonth=`date -d yesterday +%B`
 month=`date +%B`
-awk '{if($9==200)print $0}' $loglocal|sed -n "/$(date -d yesterday +%d)\/${yMonth:0:3}\/$(date -d yesterday +%Y):00/,/$(date +%d)\/${month:0:3}\/$(date +%Y):00/p"| awk '{print $1}' | sort | uniq -c | wc -l >> pv
+awk '{if($9==200)print $0}' $loglocal|sed -n "/$(date -d yesterday +%d)\/${yMonth:0:3}\/$(date -d yesterday +%Y)/,/$(date -d yesterday +%d)\/${yMonth:0:3}\/$(date -d yesterday +%Y)/p"| awk '{print $1}' | sort | uniq -c | wc -l >> pv
 echo '</center>' >> pv
 sed -i '$d' docs/README.md
 echo $(cat pv) >> docs/README.md
