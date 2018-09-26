9fbb66c311cc7e86bc8320a545c02135b829e332
gunjianpan <iofu728@163.com>
2018-09-26 14:28:47 +0800

    reset access.log

--- day.sh
+++ day.sh
@@ -1,7 +1,9 @@
+
+loglocal=/usr/local/nginx/logs/access.log
 date -d yesterday +%Y%m%d > yesterday
 yMonth=`date -d yesterday +%B`
 month=`date +%B`
-awk '{if($9==200)print $0}' /var/log/nginx/access.log|sed -n "/$(date -d yesterday +%d)\/${yMonth:0:3}\/$(date -d yesterday +%Y):00/,/$(date +%d)\/${month:0:3}\/$(date +%Y):00/p"| awk '{print $1}' | sort | uniq -c | wc -l >> yesterday
+awk '{if($9==200)print $0}' $loglocal|sed -n "/$(date -d yesterday +%d)\/${yMonth:0:3}\/$(date -d yesterday +%Y):00/,/$(date +%d)\/${month:0:3}\/$(date +%Y):00/p"| awk '{print $1}' | sort | uniq -c | wc -l >> yesterday
 echo $(cat yesterday) >> day
 
 
