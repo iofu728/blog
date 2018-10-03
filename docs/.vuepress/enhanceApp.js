bb555f86fe8c847c57263759de54295d6a662768
gunjianpan <iofu728@163.com>
2018-10-03 22:17:22 +0800

    change gitalk

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -23,30 +23,24 @@ function integrateGitalk(router) {
       }
     }
   })
-function renderGitalk(fullPath) {
-  const gitalk = new Gitalk({
-    clientID: '6ac606b7bad30bff534c',
-    clientSecret: 'cf218bccc6b17b1feaee02b406d0c1f021aaa5e7',
-    repo: 'blog',
-    owner: 'iofu728',
-    admin: ['iofu728'],
-    id: 'comment',
-    distractionFreeMode: false,
-    language: 'zh-CN',
-})
+  function renderGitalk(fullPath) {
+    const gitalk = new Gitalk({
+      clientID: '6ac606b7bad30bff534c',
+      clientSecret: 'cf218bccc6b17b1feaee02b406d0c1f021aaa5e7',
+      repo: 'blog',
+      owner: 'iofu728',
+      admin: ['iofu728'],
+      id: 'comment',
+      distractionFreeMode: false,
+      language: 'zh-CN',
+    })
 
-gitalk.render('gitalk-container')
+    gitalk.render('gitalk-container')
   }
 }
 
-export default ({
-  Vue, // VuePress 正在使用的 Vue 构造函数
-  options, // 附加到根实例的一些选项
-  router, // 当前应用的路由实例
-  siteData // 站点元数据
-}) => {
+export default ({Vue, options, router, siteData}) => {
   try {
-    // 生成静态页时在node中执行，没有document对象
     document && integrateGitalk(router)
   } catch (e) {
     console.error(e.message)
