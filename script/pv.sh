8f74656e383733bed81dbc5746965b88103ce699
gunjianpan <iofu728@163.com>
2018-10-28 21:56:18 +0800

    :calendar: change uv to pv

--- script/pv.sh
+++ script/pv.sh
@@ -11,10 +11,16 @@ cat log/yesterday | tail -n +3 | head -n 1 >> log/pv
 echo "</center>" >> log/pv
 sed -i '$d' docs/README.md
 echo $(cat log/pv) >> docs/README.md
-bash script/build.sh
+insertword=$(echo $(cat log/pv))
+pattern=\<center\>累.*\<\/center\>
+sed -i -e s@$pattern@$xxx@g ../../index.html
 
 # time total pv
-date +%Y/%m/%d:%T > log/time
-cat log/pv | tail -n +2 | head -n 1 >> log/time
-awk 'NR==FNR {a[$2]=$0} NR!=FNR {if(FNR>1&&!($1 in a)&&($9!=200||$0~/bot/||$0~/spider/||$0~/php/||$0~/taishan/||$6!~/GET/||$0~/Verification/||$0~/"-" "-"/||$0~/[gG]o/||$0~/[pP]ython/||$0~/curl/))print $0}' log/user $loglocal|sort | uniq -c | wc -l >> log/time
-echo $(cat log/time) >> log/total
+hour=date +%T
+if [ ${hour:4:1} -eq "0" ]
+then
+    date +%Y/%m/%d:%T > log/time
+    cat log/pv | tail -n +2 | head -n 1 >> log/time
+    awk 'NR==FNR {a[$2]=$0} NR!=FNR {if(FNR>1&&!($1 in a)&&($9!=200||$0~/bot/||$0~/spider/||$0~/php/||$0~/taishan/||$6!~/GET/||$0~/Verification/||$0~/"-" "-"/||$0~/[gG]o/||$0~/[pP]ython/||$0~/curl/))print $0}' log/user $loglocal|sort | uniq -c | wc -l >> log/time
+    echo $(cat log/time) >> log/total
+fi
