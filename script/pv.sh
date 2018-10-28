a6e3e4053727461ff290a3ce9d0b48b488d922bc
gunjianpan <iofu728@163.com>
2018-10-28 20:05:50 +0800

    :calendar: change uv to pv

--- script/pv.sh
+++ script/pv.sh
@@ -3,7 +3,7 @@
 # pv
 loglocal=/usr/local/nginx/logs/access.log
 echo "<center>累计访问量:" > log/pv
-awk '{if($9==200&&$0~/\/assets\/js\/app/&&$0!~/bot/&&$0!~/spider/&&$0!~/php/&&$0!~/taishan/&&$0!~/POST/&&$0!~/Verification/&&$0!~/"-" "-"/&&$0!~/Python/&&$0!~/go/&&$0!~/Go/&&$0!~/python/&&$0!~/curl/){print $0}}' $loglocal|sort | uniq -c | wc -l >> log/pv
+awk '{if($9==200&&$7~/\/assets\/js\/app/&&$0!~/bot/&&$0!~/spider/&&$0!~/php/&&$0!~/taishan/&&$6~/GET/&&$0!~/Verification/&&$0!~/"-" "-"/&&$0!~/[gG]o/&&$0!~/[pP]ython/&&$0!~/curl/){print $0}}' $loglocal|sort | uniq -c | wc -l >> log/pv
 echo "| 昨日访问量:" >> log/pv
 cat log/yesterday | tail -n +2 | head -n 1 >> log/pv
 echo "| 昨日爬虫数:" >> log/pv
@@ -16,5 +16,5 @@ bash script/build.sh
 # time total pv
 date +%Y/%m/%d:%T > log/time
 cat log/pv | tail -n +2 | head -n 1 >> log/time
-awk 'NR==FNR {a[$2]=$0} NR!=FNR {if(FNR>1&&!($1 in a)&&($9!=200||$0~/bot/||$0~/spider/||$0~/php/||$0~/taishan/||$0~/POST/||$0~/Verification/||$0~/"-" "-"/||$0~/Python/||$0~/go/||$0~/Go/||$0~/python/||$0~/curl/))print $0}' log/user $loglocal|sort | uniq -c | wc -l >> log/time
+awk 'NR==FNR {a[$2]=$0} NR!=FNR {if(FNR>1&&!($1 in a)&&($9!=200||$0~/bot/||$0~/spider/||$0~/php/||$0~/taishan/||$6!~/GET/||$0~/Verification/||$0~/"-" "-"/||$0~/[gG]o/||$0~/[pP]ython/||$0~/curl/))print $0}' log/user $loglocal|sort | uniq -c | wc -l >> log/time
 echo $(cat log/time) >> log/total
