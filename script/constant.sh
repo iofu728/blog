0ba985ce7ef994c0c5297fae523758a082e3c1f1
gunjianpan <iofu728@163.com>
2018-11-09 17:55:27 +0800

    :octocat: update script

---
+++ script/constant.sh
@@ -0,0 +1,10 @@
+# @Author: gunjianpan
+# @Date:   2018-11-09 16:09:31
+# @Last Modified by:   gunjianpan
+# @Last Modified time: 2018-11-09 17:20:58
+
+# File Path in your Service !important
+logpath=/usr/local/nginx/logs/access.log
+backuppath=/usr/local/www/wyydsb/access.log
+gitpath=/usr/local/www/wyydsb
+nginxpath=/usr/local/www
