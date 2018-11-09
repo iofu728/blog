bc01eea7a69ebde39e6067ec45698bac235c663d
gunjianpan <iofu728@163.com>
2018-11-09 13:07:29 +0800

    :octocat: update script

--- script/pv.sh
+++ script/pv.sh
@@ -19,7 +19,8 @@ if [ ${hour:4:1} -eq 0 ] && [ ${hour:6:1} -eq 0 ]
 then
     date +%Y/%m/%d:%T > log/time
     cat log/pv | tail -n +2 | head -n 1 >> log/time
-    awk 'NR==FNR {a[$2]=$0} NR!=FNR {if(FNR>1&&!($1 in a)&&($9!=200||$0~/bot/||$0~/spider/||$0~/php/||$0~/taishan/||$6!~/GET/||$0~/Verification/||$0~/"-" "-"/||$0~/[gG]o/||$0~/[pP]ython/||$0~/curl/))print $0}' log/user $loglocal|sort | uniq -c | wc -l >> log/time
+    num=`awk 'NR==FNR {a[$2]=$0} NR!=FNR {if(FNR>1&&!($1 in a)&&($9!=200||$0~/bot/||$0~/spider/||$0~/php/||$0~/taishan/||$6!~/GET/||$0~/Verification/||$0~/"-" "-"/||$0~/[gG]o/||$0~/[pP]ython/||$0~/curl/))print $0}' log/user $loglocal|sort | uniq -c | wc -l`
+    expr $num + 65352 >> log/time
     total=$(cat log/time | tail -n +2 | head -n 1)
     spider=$(cat log/time | tail -n +3 | head -n 1)
     word=$(echo $(cat log/time))
