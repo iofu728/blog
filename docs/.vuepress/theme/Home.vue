c4fedd2f32f9250b2a05c9666d5183a8f6818343
gunjianpan <iofu728@163.com>
2018-11-11 01:42:56 +0800

    :jeans: card style

--- docs/.vuepress/theme/Home.vue
+++ docs/.vuepress/theme/Home.vue
@@ -62,7 +62,7 @@ export default {
   methods: {
     getPv: function () {
       const vm = this;
-      request('https://localhost/pv.txt')
+      request('https://wyydsb.xin/pv.txt')
         .then(res => {vm.pv = res;})
         .catch(reason => console.log(reason.message));
     },
