5698b40b707af4edfb73cd98a7e7d00d2783ab28
gunjianpan <iofu728@163.com>
2018-11-26 18:15:09 +0800

    :calendar: version 4.0 ppap

--- docs/.vuepress/theme/components/Comment.vue
+++ docs/.vuepress/theme/components/Comment.vue
@@ -6,29 +6,27 @@
     </v-card>
 </template>
 <script>
-// import 'gitalk/dist/gitalk.css'
-// import Gitalk from 'gitalk'
 
   export default {
     name: '',
     mounted(){
-      // if (typeof Gitalk !== 'undefined' && Gitalk instanceof Function) {
-      //   const gitalk = new Gitalk({
-      //     clientID: '6ac606b7bad30bff534c',
-      //     clientSecret: 'cf218bccc6b17b1feaee02b406d0c1f021aaa5e7',
-      //     repo: 'blog',
-      //     owner: 'iofu728',
-      //     admin: ['iofu728'],
-      //     id: 'comment',
-      //     distractionFreeMode: false,
-      //     language: 'zh-CN',
-      //   })
-      //   try {
-      //     document && frontmatter && gitalk.render("gitalk-container");
-      //   } catch (e) {
-      //     console.error(e.message)
-      //   }
-      // }
+      if (typeof Gitalk !== 'undefined' && Gitalk instanceof Function) {
+        const gitalk = new Gitalk({
+          clientID: '6ac606b7bad30bff534c',
+          clientSecret: 'cf218bccc6b17b1feaee02b406d0c1f021aaa5e7',
+          repo: 'blog',
+          owner: 'iofu728',
+          admin: ['iofu728'],
+          id: 'comment',
+          distractionFreeMode: false,
+          language: 'zh-CN',
+        })
+        try {
+          document && gitalk.render("gitalk-container");
+        } catch (e) {
+          console.error(e.message)
+        }
+      }
     }
   }
 </script>
