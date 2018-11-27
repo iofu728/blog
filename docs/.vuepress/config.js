50ffaf2dc659fcf61f5e65c6942f7c917b2a0019
gunjianpan <iofu728@163.com>
2018-11-27 10:15:23 +0800

    :calendar: version 4.0 ppap

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,13 +11,12 @@ module.exports = {
   head: [
     ['link', {rel: 'shortcut icon', href: '/favicon.ico'}],
     ['link', {rel: 'manifest', href: '/manifest.json'}],
-    ['meta', {name: 'theme-color', content: '#3F51B5'}],
+    ['meta', {name: 'theme-color', content: '#07527a'}],
     ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
     ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
     ['link', {rel: 'apple-touch-icon', href: '/icons/192.png'}],
-    // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
     ['meta', {name: 'msapplication-TileImage', content: '/icons/192.png'}],
-    ['meta', {name: 'msapplication-TileColor', content: '#3F51B5'}]
+    ['meta', {name: 'msapplication-TileColor', content: '#07527a'}]
   ],
   serviceWorker: true,
   theme: '',
