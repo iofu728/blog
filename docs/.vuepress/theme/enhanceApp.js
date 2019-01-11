47fc42e963057107d755cc99ba58357d421321fe
gunjianpan <iofu728@163.com>
2019-01-11 19:08:48 +0800

    🎎 rollback gitalk

--- docs/.vuepress/theme/enhanceApp.js
+++ docs/.vuepress/theme/enhanceApp.js
@@ -5,7 +5,7 @@ import components from './components'
 import './styles/theme.styl'
 
 
-function getGitalk() {
+function getGitalk(router) {
   const linkGitalk = document.createElement('link');
   linkGitalk.href = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css';
   linkGitalk.rel = 'stylesheet';
@@ -13,6 +13,45 @@ function getGitalk() {
   const scriptGitalk = document.createElement('script');
   scriptGitalk.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js';
   document.body.appendChild(scriptGitalk);
+
+  router.afterEach((to) => {
+    if (scriptGitalk.onload) {
+      loadGitalk(to);
+    } else {
+      scriptGitalk.onload = () => {
+        loadGitalk(to);
+      }
+    }
+  });
+
+  function loadGitalk(to) {
+    let commentsContainer = document.getElementById('gitalk-container');
+    if (!commentsContainer) {
+      commentsContainer = document.createElement('div');
+      commentsContainer.id = 'gitalk-container';
+      commentsContainer.classList.add('content');
+    }
+    const $page = document.querySelector('.page');
+    if ($page) {
+      $page.appendChild(commentsContainer);
+      if (typeof Gitalk !== 'undefined' && Gitalk instanceof Function) {
+        renderGitalk(to.fullPath);
+      }
+    }
+  }
+  function renderGitalk(fullPath) {
+    const gitalk = new Gitalk({
+      clientID: '6ac606b7bad30bff534c',
+      clientSecret: 'cf218bccc6b17b1feaee02b406d0c1f021aaa5e7',
+      repo: 'blog',
+      owner: 'iofu728',
+      admin: ['iofu728'],
+      id: 'comment',
+      distractionFreeMode: false,
+      language: 'zh-CN',
+    });
+    gitalk.render('gitalk-container');
+  }
 }
 
 function prepare (siteData) {
@@ -43,7 +82,7 @@ export default ({ Vue, options, router, siteData }) => {
   Vue.use(routes, { router, theme })
   Vue.use(components, theme)
   try {
-    document && router !== '/404.html' && getGitalk()
+    document && getGitalk(router)
   } catch (e) {
     console.error(e.message)
   }
