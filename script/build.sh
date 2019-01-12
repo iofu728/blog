69497a403ef7f1516ad6bec4ad9665f757ae2dea
gunjianpan <iofu728@163.com>
2019-01-12 14:53:11 +0800

    🦁 test optimiza

--- script/build.sh
+++ script/build.sh
@@ -42,14 +42,17 @@ else
 rm -rf ${nginxpath}/icons
 fi
 
-for htmlfile in docs/.vuepress/dist/*/*.html docs/.vuepress/dist/*.html
+for htmlFile in docs/.vuepress/dist/*/*.html docs/.vuepress/dist/*.html
 do
-    sed -i '/<head>/a\<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />' $htmlfile
-    sed -i '/<head>/a\<meta name="google-site-verification" content="7ULbF13p7e6Z16vpi2tbAPHXHJBVu83TaxPTnvwnA8I" />' $htmlfile
-    sed -i '/<head>/a\<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113936890-1"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-113936890-1');</script>' $htmlfile
-    sed -i '/<body>/a\<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJK56VN"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>' $htmlfile
-    sed -i '/<body>/a\<script type="text/javascript"> docsearch({ apiKey: 'c42b71d494ca78750c7094eb2c55eda6', indexName: 'wyydsb', inputSelector: '', debug: false }); </script> ' $htmlfile
-    sed -i '/<body>/a\<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>' $htmlfile
+    needJs=`cat ${htmlFile} | tail -n 3 | head -n 1`
+    sed -i '/preload.*/d' ${htmlFile}
+    sed -i "/<head>/a$needJs" ${htmlFile}
+    sed -i '/<head>/a\<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />' ${htmlFile}
+    sed -i '/<head>/a\<meta name="google-site-verification" content="7ULbF13p7e6Z16vpi2tbAPHXHJBVu83TaxPTnvwnA8I" />' ${htmlFile}
+    sed -i '/<head>/a\<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113936890-1"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-113936890-1');</script>' ${htmlFile}
+    sed -i '/<body>/a\<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJK56VN"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>' ${htmlFile}
+    sed -i '/<body>/a\<script type="text/javascript"> docsearch({ apiKey: 'c42b71d494ca78750c7094eb2c55eda6', indexName: 'wyydsb', inputSelector: '', debug: false }); </script> ' ${htmlFile}
+    sed -i '/<body>/a\<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>' ${htmlFile}
 done
 
 mv docs/.vuepress/dist/* ${nginxpath}/
