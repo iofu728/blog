a025959400b57ee05cd17007e4c5f11398eb96ad
gunjianpan <iofu728@163.com>
2019-01-14 11:34:31 +0800

    😶 block ip

--- script/constant.sh
+++ script/constant.sh
@@ -2,10 +2,11 @@
 # @Author: gunjianpan
 # @Date:   2018-11-09 16:09:31
 # @Last Modified by:   gunjianpan
-# @Last Modified time: 2018-11-09 18:17:32
+# @Last Modified time: 2019-01-14 11:18:30
 
 # File Path in your Service !important
 logpath=/usr/local/nginx/logs/access.log
 backuppath=/usr/local/www/wyydsb/access.log
 gitpath=/usr/local/www/wyydsb/blog
 nginxpath=/usr/local/www
+blockpath=/usr/local/nginx/conf/block_ip.conf
