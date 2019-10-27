4e1afcfdfa194fefc243f617891900a1ac29567c
gunjianpan <iofu728@163.com>
2019-10-27 16:03:19 +0800

    🤗 fix soft link set

--- script/constant.sh
+++ script/constant.sh
@@ -2,7 +2,7 @@
 # @Author: gunjianpan
 # @Date:   2018-11-09 16:09:31
 # @Last Modified by:   gunjianpan
-# @Last Modified time: 2019-10-26 12:58:13
+# @Last Modified time: 2019-10-27 16:02:55
 
 # File Path in your Service !important
 logpath=/usr/local/tengine/logs/access.log
@@ -10,5 +10,5 @@ backuppath=/usr/local/www/wyydsb/access.log
 gitpath=/usr/local/www/wyydsb/blog
 nginxpath=/usr/local/wyydsb
 blockpath=/usr/local/tengine/conf/block_ip.conf
-NGINX=tengine
-HTTPSSET=certbot
+NGINX=/usr/local/bin/tengine
+HTTPSSET=/usr/bin/certbot
