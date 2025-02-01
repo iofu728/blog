d50783f1be6ee7e235272e1c18b90c811ab47f83
Huiqiang Jiang <iofu728@163.com>
2025-02-01 20:38:56 +0800

    Feature(blog): update

--- script/build.sh
+++ script/build.sh
@@ -15,7 +15,7 @@ for name in assets javaScript other pat tags icons NLP Spider Operations DB Codi
 done
 
 
-sed -i '/<head>/a\<script type="application/ld+json">{"@context": "https://schema.org","@type": "NewsArticle","mainEntityOfPage": {"@type": "WebPage","@id": "https://wyydsb.xin"},"headline": "乌云压顶是吧","image": "https://wyydsb.xin/face.jpg","datePublished": "2017-10-10T08:00:00+08:00","dateModified": "2019-01-01T19:24:025+08:00","author": {"@type": "Person","name": "Gun Jianpan"},"publisher": {"@type": "Organization","name": "Gun Jianpan","logo": {"@type": "ImageObject","url": "https://wyydsb.xin/favicon.ico"}},"description": "Some coder skills"}</script>' docs/.vuepress/dist/index.html
+sed -i '/<head>/a\<script type="application/ld+json">{"@context": "https://schema.org","@type": "NewsArticle","mainEntityOfPage": {"@type": "WebPage","@id": "https://wyydsb.xin"},"headline": "乌云压顶是吧","image": "https://wyydsb.xin/face.jpg","datePublished": "2017-10-10T08:00:00+08:00","dateModified": "2019-01-01T19:24:025+08:00","author": {"@type": "Person","name": "Huiqiang Jiang"},"publisher": {"@type": "Organization","name": "Huiqiang Jiang","logo": {"@type": "ImageObject","url": "https://wyydsb.xin/favicon.ico"}},"description": "Some coder skills"}</script>' docs/.vuepress/dist/index.html
 
 for htmlFile in docs/.vuepress/dist/*/*.html docs/.vuepress/dist/*.html; do
     needJs=$(cat ${htmlFile} | tail -n 3 | head -n 1)
